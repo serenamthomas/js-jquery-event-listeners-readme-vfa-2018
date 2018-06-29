b081cf3ab92d03c2e13272f4357caed51ce98fb5
Serena Thomas <serenathomas96@gmail.com>
2018-06-29 04:09:11 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,28 @@
 //define functions here
+function getIt() {
+  $("p").bind("click", function(){
+    alert("Hey!");
+});
+}
 
+//function frameIt() {
+  //$(.tasty).load
+//}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+  if(key.which == 71){
+      alert('G was pressed');
+  }
+});
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+    return;
+});
+}
 $(document).ready(function(){
 
 // call functions here
